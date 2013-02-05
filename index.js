9cac4183feb8606b9ed569a3590e920a3b627d00
Jonathan Ong <jonathanrichardong@gmail.com>
2013-02-04 19:32:42 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
 var matches = require('matches-selector')
 
-module.exports = function closest(element, selector, checkYoSelf) {
-  if (checkYoSelf && matches(element, selector))
-    return element;
+module.exports = function (element, selector, checkYoSelf) {
+  var parent = checkYoSelf ? element : element.parentNode
 
-  var parent = element.parentNode
-
-  if (element === document || parent === document) return;
-
-  if (matches(parent, selector)) return parent;
-
-  return closest(parent, selector)
+  while (parent && parent !== document) {
+    if (matches(parent, selector)) return parent;
+    parent = parent.parentNode
+  }
 }
